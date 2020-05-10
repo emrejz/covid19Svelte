b6954cc27eb60e1815acaed7d7e0699565a27caf
emrejz <emre.kayacan@msn.com>
2020-05-11 02:53:14 +0300

    comps created

---
+++ src/components/TableContainer.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Table from "./Table.svelte";
+  // // import TableChart from './TableChart.svelte'
+  import TableFilter from "./TableFilter.svelte";
+</script>
+
+<h1>Table Container</h1>
+<TableFilter />
+<Table />
