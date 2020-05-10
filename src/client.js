5d54b7b06433848578585fc42c4410685f1846f8
emrejz <emre.kayacan@msn.com>
2020-05-11 02:12:02 +0300

    bootstrap added

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
-import * as sapper from '@sapper/app';
-
+import * as sapper from "@sapper/app";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 sapper.start({
-	target: document.querySelector('#sapper')
+  target: document.querySelector("#sapper"),
 });
