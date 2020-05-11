6e49e5a5950d2c812e8ea7f5d4a72c5e36d331bb
emrejz <emre.kayacan@msn.com>
2020-05-12 00:42:35 +0300

    toggle button ok

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,46 @@
 <script>
-  export let segment;
+  // export let segment;
+  let show = false;
+  function toggleNavbar() {
+    show = !show;
+  }
 </script>
 
 <style>
-
+  .showNavbar {
+    display: block;
+  }
 </style>
 
-<nav />
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">Navbar</a>
+  <button class="navbar-toggler" type="button" on:click={toggleNavbar}>
+    <span class="navbar-toggler-icon" />
+  </button>
+
+  <div class="collapse navbar-collapse" class:showNavbar={show} id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">
+          Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/features">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/pricing">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link disabled"
+          href="/disabled"
+          tabindex="-1"
+          aria-disabled="true">
+          Disabled
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
